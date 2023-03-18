97be6040fe0ad288b3c0af1f0e2cbe68b1b357ec
seb-fae <71495014+seb-fae@users.noreply.github.com>
2023-03-18 18:57:08 +0100

    Create startupif.sh

---
+++ startupif.sh
@@ -0,0 +1,8 @@
+#Stop 4G and VPN interface
+sudo ifconfig enx001e101f0000 down
+#Retrieve an IP address from 4G interface
+sudo dhclient enx001e101f0000
+#Set DNS server when this information is missing
+sudo resolvectl dns enx001e101f0000 192.168.8.1
+#Start VPN client and connect to remote Freebox
+sudo openvpn --config /home/cerdan/config_openvpn_routed_sebnuc.ovpn  --auth-user-pass /home/cerdan/.openvpn/.mdp --auth-nocache --mute-replay-warnings --connect-retry 300 --connect-retry-max 1
